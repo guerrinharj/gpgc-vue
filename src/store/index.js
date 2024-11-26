d0e37dbeff869510ac62c476796fd26d27311df2
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2024-11-26 09:00:25 -0300

    added variable for accessing env

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,8 @@
 import { createStore } from 'vuex';
 import axios from 'axios';
 
+const API_BASE_URL = process.env.VUE_APP_API_BASE_URL;
+
 const store = createStore({
     state: {
         releases: [],
