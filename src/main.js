6437b3547b9dbb5f48917039b15178a2fd63f250
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2024-11-29 15:11:38 -0300

    changed for icons

--- src/main.js
+++ src/main.js
@@ -3,7 +3,14 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faPlay, faPause, faForward, faBackward, faStop } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faPlay, faPause, faForward, faBackward, faStop);
+
 createApp(App)
+    .component('font-awesome-icon', FontAwesomeIcon)
     .use(router)
-    .use(store) // Use the store here
+    .use(store)
     .mount('#app');
