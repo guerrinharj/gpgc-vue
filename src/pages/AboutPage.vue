305023a98d610e6326c3ea15bb89f0b10003c1ed
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2026-06-03 11:01:17 -0300

    new bio

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,13 +1,9 @@
 <template>
     <div class="about-page">
-        <div class="content-wrapper">
-            <p
-                v-for="(paragraph, index) in paragraphs"
-                :key="index"
-                class="paragraph"
-                v-html="paragraph"
-            ></p>
-        </div>
+        <div
+            class="content-wrapper"
+            v-html="aboutText"
+        ></div>
     </div>
 </template>
 
@@ -17,61 +13,22 @@ export default {
 
     data() {
         return {
-            paragraphs: [
-                `Hi, my name is <b>Gabriel Pessoa Guerra Cavalcanti</b>`,
-                `I'm a musician, producer and web developer.`,
-                `The purpose of this site is to catalog and serve as a streaming player to everything I’ve done related to <b>"music"</b>.`,
-                `You can also download it.`,
-                `As the years went by, and I got more and more cursed by this abstract entity called <b>"music"</b>`,
-                `I've made the questionable choice to constantly release stuff under different groups, pseudonyms, aliases, featurings, soundtracks etc. Making things harder to catalog.`,
-                `So we're gonna <b>clarify it</b>.`,
-                `Not everything still exists, some files and links just got lost or broken.`,
-                `But they are documented here.`,
-                `This website was built with <b>Vue</b>, powered by a <b>Ruby on Rails API</b> (<a href="http://gpgc-api.onrender.com">here's the documentation</a>). You can check the source codes from the front <a href="https://github.com/guerrinharj/gpgc-vue">here</a> and the back <a href="https://github.com/guerrinharj/gpgc-api">here</a>.`,
-                `If you’d like to contact me, please <b><a href="mailto:gabrielpessoaguerracavalcanti@gmail.com">send an email</a></b>.`
-            ]
+            aboutText: `
+                <p>Hi, my name is <b>Gabriel Pessoa Guerra Cavalcanti</b>. I'm a musician, producer and web developer and the purpose of this site is to catalog and serve as a streaming player to everything I've done related to <b>music</b>. 
+                <p>As the years went by, and I got more and more cursed by this abstract entity called <b>music</b>, I've made the questionable choice to constantly release stuff under different groups, pseudonyms, aliases, featurings, soundtracks etc., making things harder to catalog. I've also released under different labels such as Future Times, PAN, Superconscious Records, 2 Headed Deer, Confuso Editions, etc and managed labels such as 40% Foda/Maneirissimo and <a href="gpgc.info/label>"GPGC</a>.</p>
+                <p>To clarify, not everything still exists; some files and links just got lost or broken, but they are documented here. </p>
+                <p>This website was built with <b>Vue</b>, powered by a <b>Ruby on Rails API</b> (<a href="http://gpgc-api.onrender.com">documentation</a>). You can check the source code from the front <a href="https://github.com/guerrinharj/gpgc-vue">here</a> and the back <a href="https://github.com/guerrinharj/gpgc-api">here</a>. If you'd like to contact me, please <a href="mailto:gabrielpessoaguerracavalcanti@gmail.com"><b>send an email</b></a>.</p>
+            `
         };
     }
 };
 </script>
 
 <style>
-.about-page {
-    width: 100vw;
-    min-height: 100vh;
-    background: black;
-    color: white;
-    font-size: 24px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    padding: 4rem 0;
-}
-
 .content-wrapper {
-    width: 100%;
-    max-width: 900px;
-    padding: 0 2rem;
-    text-align: center;
-}
-
-.paragraph {
-    margin: 0 0 1.5rem;
-}
-
-.about-page a {
-    font-weight: 600;
-    color: white;
-}
-
-.about-page a:hover {
-    text-decoration: underline;
-}
-
-@media (max-width: 768px) {
-    .about-page {
-        font-size: 1.2rem;
-        letter-spacing: -1px;
-    }
+    max-width: 800px;
+    text-align: left;
+    line-height: 1.8;
+    padding: 2rem;
 }
 </style>
